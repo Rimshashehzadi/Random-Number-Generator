c6815995030852b11e60d93ca53546dbde775538
rimsha_shehzadi <rimshashehzadi79@gmail.com>
2024-08-16 16:12:50 +0500

    random number generator

---
+++ index.js
@@ -0,0 +1,17 @@
+let x;
+let y;
+let z;
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+document.getElementById("rollbutton").onclick = function(){
+    
+    x = Math.floor(Math.random() * 20) + 1;
+    y = Math.floor(Math.random() * 20) + 1;
+    z = Math.floor(Math.random() * 20) + 1;
+document.getElementById("xlabel").innerHTML = x;
+document.getElementById("ylabel").innerHTML = y;
+document.getElementById("zlabel").innerHTML = z;
+
+}
